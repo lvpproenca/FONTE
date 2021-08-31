c6f9b407e58dfc0f554fe7b78659dfb4b0101f94
lvpproenca <lvp.proenca@gmail.com>
2021-08-31 16:36:42 -0300

    Initial commit

---
+++ VENDAS.dpr
@@ -0,0 +1,26 @@
+program VENDAS;
+
+uses
+  Vcl.Forms,
+  UVendas in 'UVendas.pas' {FrmVendas},
+  UDM in 'UDM.pas' {DM: TDataModule},
+  Classe.Conexao in 'Classe.Conexao.pas',
+  System.SysUtils,
+  ClassePedidos in 'ClassePedidos.pas',
+  UPesquisaClientes in 'UPesquisaClientes.pas' {frmPesquisaClientes},
+  UConsultaProdutos in 'UConsultaProdutos.pas' {frmConsultaProdutos};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TDM, DM);
+  if DM.Conexao.ConectarBanco then
+  begin
+    Application.CreateForm(TFrmVendas, FrmVendas);
+    Application.Run;
+  end
+  else
+    raise exception.Create('Não foi possível realizar a conexão com o banco de dados!'+#13+'Aplicação finalizada.');
+end.
